04d07e98eec8c5217343a82e04c89babdfd0864b
Leah <leahmlsison@gmail.com>
2017-11-15 06:11:18 +0800

    Edited Assets

--- Assets.java
+++ Assets.java
@@ -5,6 +5,16 @@ public class Assets {
     static BufferedImage imRooster;
 
     static BufferedImage background;
+    static BufferedImage gameScreen;
+    static BufferedImage tutorial1;
+    static BufferedImage tutorial2;
+    static BufferedImage tutorial3;
+    static BufferedImage tutorial4;
+
+    static BufferedImage countdown_1;
+    static BufferedImage countdown_2;
+    static BufferedImage countdown_3;
+    
     static BufferedImage title;
 
     // start = new JButton(new ImageIcon(filename_source));
@@ -14,7 +24,18 @@ public class Assets {
 
 
        background = loadImage("screen");
+       gameScreen = loadImage("gamescreen");
        title = loadImage("title");
+
+       tutorial1 = loadImage("tutorial1");
+       tutorial2 = loadImage("tutorial2");
+       tutorial3 = loadImage("tutorial3");
+       tutorial4 = loadImage("tutorial4");
+
+       countdown_1 = loadImage("countdown_1");
+       countdown_2 = loadImage("countdown_2");
+       countdown_3 = loadImage("countdown_3");
+
     }
 
     public static BufferedImage loadImage(String filename) {
