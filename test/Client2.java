cb882b78df22f7b1b9cba32eaf9789a2924ba5e9
Brixjohn <brlumabi@up.edu.ph>
2017-11-21 17:17:21 +0800

    Added collision detection

---
+++ test/Client2.java
@@ -0,0 +1,227 @@
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Client implements Runnable{
+	private boolean connected;
+	private static JPanel screenDeck = new JPanel(new CardLayout());
+	private static JFrame frame;
+	static Thread t;
+	static DatagramSocket socket;
+	private static Socket serverSocket;
+	private static DataOutputStream out;
+	private static DataInputStream in;
+
+	public Client(String serverIP, int port, String name){
+		try{
+			socket = new DatagramSocket();		
+			serverSocket = new Socket(serverIP, port);
+
+			System.out.println("Just connected to " + serverSocket.getRemoteSocketAddress());
+
+			OutputStream outToServer = serverSocket.getOutputStream();
+            out = new DataOutputStream(outToServer);
+            
+		}catch(SocketException e){
+			e.printStackTrace();
+			System.exit(1);
+		}catch(UnknownHostException e){
+			e.printStackTrace();
+			System.exit(1);
+		}catch(IOException e){
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		t = new Thread(this);
+		t.start();
+		frame = new JFrame("Champions of Motunui : "+name);
+		Container c = frame.getContentPane();
+		frame.setPreferredSize(new Dimension(900,700));
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		JPanel tutorialScreen = new JPanel();
+		JPanel gameScreen = new JPanel();
+
+		tutorialScreen.setLayout(new BorderLayout());
+		JPanel southTutorial = new JPanel();
+		JButton skipTutorial = new JButton("Skip Tutorial");
+		JButton prev = new JButton("< Previous");
+		skipTutorial.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				CardLayout p = (CardLayout)screenDeck.getLayout();
+				p.show(screenDeck, "GAME");
+			}
+		});
+
+		JButton next = new JButton("Next >");
+		southTutorial.setLayout(new BorderLayout());
+		southTutorial.add(prev, BorderLayout.WEST);
+		southTutorial.add(next, BorderLayout.EAST);
+		southTutorial.add(skipTutorial, BorderLayout.CENTER);
+
+		tutorialScreen.add(new JLabel("insert tutorial here"), BorderLayout.CENTER);
+		tutorialScreen.add(southTutorial, BorderLayout.SOUTH);
+		screenDeck.add(gameScreen, "GAME");
+		screenDeck.add(tutorialScreen, "TUTORIAL");
+		
+		gameScreen.setLayout(new BorderLayout());
+		
+
+		JPanel infoBox = new JPanel();
+		infoBox.setLayout(new BorderLayout());
+		JPanel chatBox = new JPanel();
+		chatBox.setLayout(new BorderLayout());
+		JTextArea textarea = new JTextArea("WELCOME TO THE CHAT ROOM!\n");
+		textarea.setEditable(false);
+		
+		JScrollPane pane = new JScrollPane(textarea);
+		pane.setPreferredSize(new Dimension(450, 100));
+		chatBox.add(pane, BorderLayout.NORTH);
+
+		JPanel messageBox = new JPanel();
+		messageBox.setLayout(new BorderLayout());
+		JLabel l1 = new JLabel("You:");
+		JTextField message = new JTextField();
+		JButton sendButton = new JButton("Send!");
+		
+		messageBox.add(l1, BorderLayout.WEST);
+		message.setPreferredSize(new Dimension(350,20));
+		messageBox.add(message, BorderLayout.CENTER);
+		messageBox.add(sendButton, BorderLayout.EAST);
+
+		chatBox.add(messageBox, BorderLayout.EAST);
+		infoBox.add(chatBox, BorderLayout.WEST);
+
+		JPanel info = new JPanel();
+		info.setLayout(new BorderLayout());
+		JPanel lifePanel = new JPanel();
+		JButton focus = new JButton("FOCUS!");
+		
+		lifePanel.add(new JLabel("Lives left:"), BorderLayout.WEST);
+		JTextField lives = new JTextField("3");
+		lives.setEditable(false);
+		lifePanel.add(lives, BorderLayout.CENTER);
+		lifePanel.add(focus, BorderLayout.EAST);
+		info.add(lifePanel, BorderLayout.NORTH);
+
+		JTextArea leaderboard = new JTextArea("LEADERBOARD:");
+		leaderboard.setPreferredSize(new Dimension(445, 100));
+		leaderboard.setEditable(false);
+		info.add(leaderboard, BorderLayout.SOUTH);
+
+		infoBox.add(info, BorderLayout.EAST);
+
+
+		JPanel gameProper = new JPanel();
+
+		JTextArea keystrokes = new JTextArea("press a key.");
+		keystrokes.setEditable(false);
+		JScrollPane pane1 = new JScrollPane(keystrokes);
+		pane1.setPreferredSize(new Dimension(900, 520));
+
+		keystrokes.requestFocus();
+		keystrokes.addKeyListener(new KeyListener(){
+			public void keyPressed(KeyEvent ke) {}
+			public void keyTyped(KeyEvent ke) {
+				try{
+					if(ke.getKeyChar() == KeyEvent.VK_1){
+						keystrokes.setText(keystrokes.getText() + "\n"+name+" left");
+						out.writeUTF("game " + name + " left");
+
+					}else if(ke.getKeyChar() == KeyEvent.VK_0){
+						keystrokes.setText(keystrokes.getText() + "  \n"+name+" right");
+						out.writeUTF("game " + name + " right");
+
+					}else if(ke.getKeyChar() == KeyEvent.VK_SPACE){
+						keystrokes.setText(keystrokes.getText() + "  \n"+name+" FIRE");
+						out.writeUTF("game " + name + " FIRE");
+
+					}
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}
+			public void keyReleased(KeyEvent ke) {}
+		});
+
+		gameScreen.add(pane1, BorderLayout.NORTH);
+		gameScreen.add(infoBox, BorderLayout.SOUTH);
+
+
+		c.add(screenDeck);
+
+		sendButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				if(message.getText() != "" || message.getText() != null){
+					textarea.setText(textarea.getText()+"\n"+name+": "+message.getText());
+					try{
+						out.writeUTF("chat " + name + " " + message.getText());
+					}catch(IOException er){
+						er.printStackTrace();
+					}
+					message.setText("");
+				}
+				keystrokes.requestFocus();
+			}
+		});
+
+		focus.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				keystrokes.requestFocus();
+			}
+		});
+
+
+		frame.pack();
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+
+	}
+
+	public static void main(String[] args){
+		try{
+			String serverIP = args[0];
+			int port = Integer.parseInt(args[1]);
+			String name = args[2];
+			//usage: java Client <ip address of server> <port number> <name of player>		
+			Client client = new Client(serverIP, port, name);
+	        
+		}catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Usage: java GreetingClient <server ip> <port no.> <your name>");
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Cannot find (or disconnected from) Server");
+        }
+
+	}
+
+	public void run(){
+		connected = true;
+		while(connected){
+			try{
+				InputStream inFromServer = serverSocket.getInputStream();
+	            DataInputStream in = new DataInputStream(inFromServer);
+	            System.out.println(in.readUTF());
+			}catch(IOException er){
+				er.printStackTrace();
+			}
+		}
+		try{
+			serverSocket.close();	
+		}catch(IOException e){
+
+		}
+	}
+
+	public void update(String msg){
+
+	}
+}
---
+++ test/Server2.java
@@ -0,0 +1,87 @@
+import java.net.*;
+import java.io.*;
+
+public class Server extends Thread{
+	private ServerSocket serverSocket;
+	private Socket[] clients;
+	private int maxPlayers = 0;
+    static DatagramSocket serverDataSocket = null;
+    static final int WAITING_FOR_PLAYERS = 1;
+    static final int GAME_START = 2;
+    static final int ONGOING = 3;	
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
+	public Server(int port, int num) throws IOException{
+		serverSocket = new ServerSocket(port);
+		this.maxPlayers = num;
+		clients = new Socket[num];
+		System.out.println("Server is running at port "+port+"...");
+	}
+
+	public void run(){
+		boolean connected = true;
+		int playerCount = 0;
+		int stage = WAITING_FOR_PLAYERS;
+		String playerData;
+		while(true){
+			switch(stage){
+				case WAITING_FOR_PLAYERS:
+					System.out.println("WAITING_FOR_PLAYERS");
+					try{
+						clients[playerCount] = serverSocket.accept();
+						System.out.println("Just connected to player on "+clients[playerCount].getRemoteSocketAddress());
+						playerCount++;
+					}catch(IOException e){
+		                e.printStackTrace();
+		                System.out.println("Input/Output Error!");
+		                break;
+		            }
+		            if(playerCount >= maxPlayers){
+		            	stage = GAME_START;
+		            	System.out.println("GAME HAS STARTED.");
+		            }
+					break;
+				case GAME_START:
+					for(int i=0;i<maxPlayers;i++){
+						if(clients[i] != null){
+							try{
+								in = new DataInputStream(clients[i].getInputStream());
+		            			System.out.println(in.readUTF()); 	
+		            			for(int j=0;j<maxPlayers;j++){
+									if(clients[i] != null){
+										out = new DataOutputStream(clients[i].getOutputStream());
+										out.writeUTF(in.readUTF());
+									}
+								}
+							}catch(IOException e){
+								e.printStackTrace();
+							}
+						}
+					}
+
+					break;
+			}
+			
+		}
+
+	}
+
+	public static void main(String[] args){
+		try{
+            int port = Integer.parseInt(args[0]);
+            int num = Integer.parseInt(args[1]);
+            Thread t = new Server(port, num);
+            t.start();
+        }catch(IOException e){
+            //e.printStackTrace();
+            System.out.println("Usage: java Server <port no.> <no. of players>\n"+
+                    "Make sure to use valid ports (greater than 1023)");
+        }catch(ArrayIndexOutOfBoundsException e){
+            //e.printStackTrace();
+            System.out.println("Usage: java Server <port no.> <no. of players>\n"+
+                    "Insufficient arguments given.");
+        }
+
+	}
+}
